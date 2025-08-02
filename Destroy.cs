135bff5c818da71e8aa74b3b0708d980e7abaeda
KseniiaAlekseeva291 <alexeeva.kseniia@gmail.com>
2025-08-02 13:34:59 +0300

    Destroys an object at the start of the game

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroys the object at the start of the game
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
