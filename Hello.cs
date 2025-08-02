72c722e330d836134837d7299689a1995cc57cb3
KseniiaAlekseeva291 <alexeeva.kseniia@gmail.com>
2025-08-02 13:22:53 +0300

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        // Вывод в консоль привет мир 
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
